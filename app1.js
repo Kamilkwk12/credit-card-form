ded8fcd79c44e5e65b69e3b7cbe6e0c0123d627f
Kamil <115706153+Kamilkwk12@users.noreply.github.com>
2022-11-07 20:06:18 +0100

    Add files via upload

--- app1.js
+++ app1.js
@@ -1,5 +1,4 @@
-
-// cleaning fields onload
+// cleaning form onload
 const inputs = document.getElementsByTagName('input');
 
 window.onload = function() {
@@ -8,6 +7,61 @@ window.onload = function() {
         selectedInput.value = '';
     }
 }
+
+// error messages variables
+const form = document.getElementById('form');
+const errorName = document.getElementById('error-name');
+const errorNumber = document.getElementById('error-number');
+const errorExpDate = document.getElementById('error-exp');
+const errorCvv = document.getElementById('error-cvv');
+
+
+// functions locking number or letter inputs in fields
+function checkNumbers(event) {
+    var keyId = event.which;
+    if (keyId >= 48 && keyId <= 57) {
+        errorName.textContent = "Numbers aren't allowed in this field"; 
+        event.returnValue = false;
+    }
+    else {
+        event.returnValue = true;
+    }
+}
+
+function checkLetters(event, fieldId) {
+    var keyId = event.which;
+    if (!(keyId >= 48 && keyId <= 57 || keyId == 8)) {
+        if (fieldId == '1') {
+            errorNumber.textContent = "Letters aren't allowed in this field";
+        }
+        if (fieldId == '2') {
+            errorExpDate.textContent = "Letters aren't allowed";
+        }
+        if (fieldId == '3') {
+            errorExpDate.textContent = "Letters aren't allowed";
+        }
+        if (fieldId == '4') {
+            errorCvv.textContent = "Letters aren't allowed";
+        }
+        event.returnValue = false;
+    }
+    else {
+        if (fieldId == '1') {
+            errorNumber.textContent = "";
+        }
+        if (fieldId == '2') {
+            errorExpDate.textContent = "";
+        }
+        if (fieldId == '3') {
+            errorExpDate.textContent = "";
+        }
+        if (fieldId == '4') {
+            errorCvv.textContent = "";
+        }
+        event.returnValue = true;
+    }
+}
+
 
 // carholder name uptade on card and check if name and surname are typed correctly
 const cardHolderInput = document.getElementById('cardholder-name');
@@ -29,7 +83,6 @@ function nameOnCard(e) {
         if(regExArray.length >= 1) {
             errorName.textContent = "Numbers aren't allowed in this field";
         }
-        
         else {
             errorName.textContent = '';
             cardHolderOutput.innerHTML = e.target.value;
@@ -45,12 +98,18 @@ cardHolderInput.addEventListener('blur', function() {
         cardHolderInput.style.borderColor = 'green';
     }
     else {
-        cardHolderInput.style.borderColor = 'red';
-        errorName.innerHTML = 'Wrong format - enter required format'
+        if(nameSurname == '') {
+            cardHolderInput.style.borderColor = 'red';
+            errorName.innerHTML = "Field can't be blank";
+        }
+        else {
+            cardHolderInput.style.borderColor = 'red';
+            errorName.innerHTML = 'Wrong format - enter required format';
+        }
     }
 })
 
-
+// card number uptade on card and validation
 const cardNumberOutput = document.getElementById('card-number-on-card');
 const cardNumberInput = document.getElementById('card-number');
 
@@ -60,7 +119,6 @@ cardNumberInput.addEventListener('input', numberOnCard);
 function numberOnCard(e) { 
     if(e.target.value == ''){
         cardNumberOutput.innerHTML = '1234 5678 9123 4000';
-
     }
     else{
         errorNumber.textContent = '';
@@ -76,8 +134,14 @@ function numberOnCard(e) {
 
 cardNumberInput.addEventListener('blur', function() {
     if(cardNumberInput.value.length != 16) {
-        cardNumberInput.style.borderColor = 'red';
-        errorNumber.innerHTML = 'Wrong format - enter 16 digit number';
+        if(cardNumberInput.value == '') {
+            cardNumberInput.style.borderColor = 'red';
+            errorNumber.innerHTML = "Field can't be blank";
+        }
+        else{
+            cardNumberInput.style.borderColor = 'red';
+            errorNumber.innerHTML = 'Wrong format - enter 16 digit number';
+        }
     }
     else {
         cardNumberInput.style.borderColor = 'green';
@@ -86,6 +150,8 @@ cardNumberInput.addEventListener('blur', function() {
 
 })
 
+
+// expiration date update on card and validation
 const month = document.getElementById('expiration-month');
 const year = document.getElementById('expiration-year');
 const dateOutput = document.getElementById('date-on-card');
@@ -93,16 +159,12 @@ const dateOutput = document.getElementById('date-on-card');
 month.addEventListener('input', dateOnCard);
 year.addEventListener('input', dateOnCard);
 
-function dateOnCard(e) {
+function dateOnCard() {
     if(month.value == '' && year.value == '') {
         dateOutput.innerHTML = '00/00';
     }
     else{
-        // errorExpDate.textContent = '';
         dateOutput.innerHTML = month.value + '/' + year.value;
-    }
-    if(e.target.length = 2) {
-        // errorExpDate.textContent = '';
     }
 }
 
@@ -111,40 +173,63 @@ year.addEventListener('blur', yearFormatCheck);
 
 function monthFormatCheck() {
     if (month.value.length != 2) {
-        errorExpDate.textContent = 'Enter date correctly'
-        month.style.borderColor = 'red'
+        if(month.value == '') {
+            errorExpDate.textContent = "Field can't be blank";
+            month.style.borderColor = 'red'
+        }
+        else{
+            errorExpDate.textContent = 'Enter date correctly'
+            month.style.borderColor = 'red'
+        }
     }
     else {
         var monthNumber = Number(month.value);
         if (monthNumber >= 1 && monthNumber <=12){
-            errorExpDate.textContent = ''
-            month.style.borderColor = 'green'
+            if (!year.style.borderColor == "red") {
+                errorExpDate.textContent = '';
+            }
+            else{
+                month.style.borderColor = 'green'
+            }
         }
         else {
             errorExpDate.textContent = 'Invalid date'
             month.style.borderColor = 'red'
         }
+        if (month.style.borderColor == 'green' && year.style.borderColor == 'green') {
+            errorExpDate.textContent = '';
+        }
     }
 }
+
 function yearFormatCheck() {
     if (year.value.length != 2) {
-        errorExpDate.textContent = 'Enter date correctly'
-        year.style.borderColor = 'red'
+        if(year.value == '') {
+            errorExpDate.textContent = "Field can't be blank";
+            year.style.borderColor = 'red'
+        } 
+        else {
+            errorExpDate.textContent = 'Enter date correctly'
+            year.style.borderColor = 'red'
+        }
     }
     else {
         var yearNumber = Number(year.value);
         if (yearNumber >= 22 && yearNumber <=99){
-            errorExpDate.textContent = ''
-            year.style.borderColor = 'green'
+                year.style.borderColor = 'green';
         }
         else {
             errorExpDate.textContent = 'Invalid date'
             year.style.borderColor = 'red'
         }
+        if (month.style.borderColor == 'green' && year.style.borderColor == 'green') {
+            errorExpDate.textContent = '';
+        }
     }
 }
 
 
+// cvv update on card and validation
 const cvvInput = document.getElementById('cvv');
 const cvvOutput = document.getElementById('cvv-on-card');
 
@@ -159,10 +244,16 @@ function cvvOnCard(e) {
     }
 }
 
-cvvInput.addEventListener('blur', function(e) {
+cvvInput.addEventListener('blur', function() {
     if(cvvInput.value.length !== 3) {
-        cvvInput.style.borderColor = 'red';
-        errorCvv.textContent = "Enter 3 digit number";
+        if(cvvInput.value == '') {
+            cvvInput.style.borderColor = 'red';
+            errorCvv.textContent = "Field can't be blank";
+        }
+        else {
+            cvvInput.style.borderColor = 'red';
+            errorCvv.textContent = "Enter 3 digit number";
+        }
     }
     else{
         cvvInput.style.borderColor = 'green';
@@ -171,38 +262,35 @@ cvvInput.addEventListener('blur', function(e) {
 })
 
 
-const form = document.getElementById('form');
-const errorName = document.getElementById('error-name');
-const errorNumber = document.getElementById('error-number');
-const errorExpDate = document.getElementById('error-exp');
-const errorCvv = document.getElementById('error-cvv');
-
-
-
-
+// submit validations and saving date from a form in localstorage to 
+// extract it in complete.html
 form.addEventListener('submit', validateData);
 
 function validateData(event) {
-
     if (cardHolderInput.value == '' || cardHolderInput.value == null) {
+        cardHolderInput.style.borderColor = 'red'
         errorName.textContent = "Field can't be blank";
         event.preventDefault();
     }
 
-    if(errorName.textContent != "" ) {
-        event.preventDefault();
-    }
-
     if (cardNumberInput.value == '' || cardNumberInput.value == null) {
+        cardNumberInput.style.borderColor = 'red'
         errorNumber.textContent = "Field can't be blank";
         event.preventDefault();
     }
 
-    if (month.value == '' || month.value == null || year.value == '' || year.value == null) {
+    if (month.value == '' || month.value == null) {
+        month.style.borderColor = 'red'
+        errorExpDate.textContent = "Field can't be blank";
+        event.preventDefault();
+    }
+    if (year.value == '' || year.value == null) {
+        year.style.borderColor = 'red'
         errorExpDate.textContent = "Field can't be blank";
         event.preventDefault();
     }
     if (cvvInput.value == '' || cvvInput.value == null) {
+        cvvInput.style.borderColor = 'red'
         errorCvv.textContent = "Field can't be blank";
         event.preventDefault();
     }
@@ -225,7 +313,6 @@ function validateData(event) {
     window.localStorage.setItem('month-value', monthValue);
     window.localStorage.setItem('year-value', yearValue);
     window.localStorage.setItem('cvv-value', cvvValue);
-
 }
 
 
